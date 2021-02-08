8fe543f4f760da3f05ae0f967b8e718e6987c261
KARIM <nabil.karim@capgemini.com>
2021-02-08 17:53:25 +0100

    project website with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitreComponent } from './components/titre/titre.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PatisserieComponent } from './components/patisserie/patisserie.component';
+import { CuisineComponent } from './components/cuisine/cuisine.component';
+import { SaladeComponent } from './components/salade/salade.component';
+import { RegisterComponent } from './contact/register/register.component';
+import { LoginComponent } from './contact/login/login.component';
+import { ShowallComponent } from './components/showall/showall.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitreComponent,
+    NavbarComponent,
+    PatisserieComponent,
+    CuisineComponent,
+    SaladeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ShowallComponent
   ],
   imports: [
     BrowserModule,
